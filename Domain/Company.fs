2475149b1ce35bcf9d669bf446760fde0a7b5379
Tamizhvendan S <tamizh88@gmail.com>
2014-12-15 17:34:58 +0530

    Adding manufacture domain

---
+++ Domain/Company.fs
@@ -0,0 +1,31 @@
+﻿namespace PhoneCat.Domain
+
+[<AutoOpen>]
+module Company = 
+    type Company = 
+        Samsung | Motorola | Dell | LG | TMobile | Sanyo | Others
+        static member ToString company =
+            match company with
+            | Samsung -> "Samsung"
+            | Motorola -> "Motorola"
+            | Dell -> "Dell"
+            | LG -> "LG"
+            | TMobile -> "TMobile"
+            | Sanyo -> "Sanyo"
+            | Others -> "Others"
+
+        static member ToCompany (name : string) =
+            match name.ToLower() with
+            | n when n.Contains("samsung") -> Samsung
+            | n when n.Contains("motorola") -> Motorola
+            | n when n.Contains("dell") -> Dell
+            | n when n.Contains("lg") -> LG
+            | n when n.Contains("tmobile") -> TMobile
+            | n when n.Contains("sanyo") -> Sanyo
+            | n when n.Contains("nexus") -> Samsung
+            | _ -> Others
+
+    type Manufacturer = 
+        { Name : string
+          Phones : seq<PhoneTypeProvider.Root> }
+
--- Domain/Phones.fs
+++ Domain/Phones.fs
@@ -2,21 +2,29 @@
 
 [<AutoOpen>]
 module Phones = 
+    type TopSellingPhone = 
+        { Id : string
+          Name : string
+          Description : string
+          ImageUrl : string }  
     
-    type TopSellingPhone = {
-        Id : string
-        Name : string
-        Description : string
-        ImageUrl : string
-    }
-    
-
-    let getTopSellingPhones (phonesSold : seq<PhoneSold>)(phones : seq<PhoneTypeProvider.Root>)  = 
+    let getTopSellingPhones (phonesSold : seq<PhoneSold>) (phones : seq<PhoneTypeProvider.Root>) = 
         phonesSold
         |> Seq.map (fun p -> p.Quantity, (phones |> Seq.find (fun p' -> p'.Id = p.Id)))
-        |> Seq.sortBy (fun x -> (~-) (fst x))
+        |> Seq.sortBy (fun x -> -(fst x))
         |> Seq.take 4
         |> Seq.map snd
-        |> Seq.map (fun x -> { Id = x.Id; Name = x.Name; Description = x.Description; ImageUrl = x.Images.[0] })
-
-
+        |> Seq.map (fun x -> 
+               { Id = x.Id
+                 Name = x.Name
+                 Description = x.Description
+                 ImageUrl = x.Images.[0] })
+    
+    let getManufacturers (phones : seq<PhoneTypeProvider.Root>) = 
+        let manufactureName = Company.ToCompany >> Company.ToString
+        phones
+        |> Seq.map (fun p -> (manufactureName p.Name), p)
+        |> Seq.groupBy fst
+        |> Seq.map (fun (key, values) -> 
+               { Name = key
+                 Phones = (values |> Seq.map snd) })
