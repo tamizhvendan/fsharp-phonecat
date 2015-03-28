82d19441333c98419f00dad3241b5dbbcd864c3a
Tamizhvendan S <tamizh88@gmail.com>
2015-03-28 15:05:51 +0530

    Refactoring

--- Web/Controllers/CheckoutController.fs
+++ Web/Controllers/CheckoutController.fs
@@ -4,18 +4,17 @@ open PhoneCat.Domain
 open PhoneCat.Domain.ShoppingCart
 
 type CheckoutController
-    (
-        getPhones : seq<string> -> seq<Phone>,
-        cart : Cart
-    ) = 
-    inherit Controller ()
+  (
+      getPhones : seq<string> -> seq<Phone>,
+      cart : Cart
+  ) = 
+  inherit Controller ()
 
-    member this.ViewCart () =
-      let phones = 
-          match cart with
-          | Empty -> Seq.empty
-          | Active xs -> getPhones xs
-            
-      this.View(phones)
+  member this.ViewCart () =
+    cart
+    |> getItems 
+    |> getPhones
+    |> this.View
+
 
     
